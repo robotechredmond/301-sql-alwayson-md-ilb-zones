0a0f345abc88036a5fd113c61bf167adbe2ded20
Keith Mayer <keith@keithmayer.com>
2018-02-05 12:16:08 -0500

    Updated SqlServerPowerShell resource

--- dsc/config-sqlao.ps1/ConfigSQLAO.ps1
+++ dsc/config-sqlao.ps1/ConfigSQLAO.ps1
@@ -132,9 +132,9 @@ configuration ConfigSQLAO
 
         Script SqlServerPowerShell
         {
-            SetScript = "Install-PackageProvider -Name NuGet -Force; Install-Module -Name SqlServer -AllowClobber -Force; Import-Module -Name SqlServer -ErrorAction SilentlyContinue"
-            TestScript = "Import-Module -Name SqlServer -ErrorAction SilentlyContinue; if (Get-Module -Name SqlServer) { $True } else { $False }"
-            GetScript = "Import-Module -Name SqlServer -ErrorAction SilentlyContinue; @{Ensure = if (Get-Module -Name SqlServer) {'Present'} else {'Absent'}}"
+            SetScript = 'Install-PackageProvider -Name NuGet -Force; Install-Module -Name SqlServer -AllowClobber -Force; Import-Module -Name SqlServer -ErrorAction SilentlyContinue'
+            TestScript = 'Import-Module -Name SqlServer -ErrorAction SilentlyContinue; if (Get-Module -Name SqlServer) { $True } else { $False }'
+            GetScript = 'Import-Module -Name SqlServer -ErrorAction SilentlyContinue; @{Ensure = if (Get-Module -Name SqlServer) {"Present"} else {"Absent"}}'
         }
 
         xWaitForADDomain DscForestWait 
